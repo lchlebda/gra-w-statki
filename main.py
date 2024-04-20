285ec1536d937e7edcf020f4d6b164004b390234
aitseb <lchlebda@gmail.com>
2024-04-20 19:40:20 +0200

    początek drugiej rundy

--- main.py
+++ main.py
@@ -5,29 +5,54 @@ input.on_button_pressed(Button.A, on_button_pressed_a)
 
 def copyShips():
     for x in range(5):
+        array[x] = []
         for y in range(5):
-            pass
+            array[x][y] = led.point(x, y)
+def drawShips():
+    for x2 in range(5):
+        for y2 in range(5):
+            if array[x2][y2]:
+                led.plot(x2, y2)
 
 def on_logo_pressed():
-    global B, A
     led.toggle(A, B)
-    B = 0
-    A = 0
 input.on_logo_event(TouchButtonEvent.PRESSED, on_logo_pressed)
 
 def on_button_pressed_ab():
-    global A, B, isPhase2
-    radio.send_string("ooo")
+    global isPhase2, A, B
+    if isPhase2:
+        radio.send_value("SHOT", 5 * B + A)
+    radio.send_string("READY")
+    copyShips()
+    createABArray()
+    basic.clear_screen()
+    isPhase2 = True
     A = 0
     B = 0
-    isPhase2 = True
-    copyShips()
 input.on_button_pressed(Button.AB, on_button_pressed_ab)
+
+def createABArray():
+    for y3 in range(5):
+        abArray[y3] = []
+        for x3 in range(5):
+            abArray[y3][x3] = 5 * y3 + x3
 
 def on_received_string(receivedString):
     global isFirstPlayer
     if receivedString == "READY":
-        isFirstPlayer = False
+        music.play(music.builtin_playable_sound_effect(soundExpression.hello),
+            music.PlaybackMode.UNTIL_DONE)
+        if isPhase2:
+            isFirstPlayer = True
+            radio.send_string("START")
+            basic.show_string("START")
+            music.play(music.builtin_playable_sound_effect(soundExpression.happy),
+                music.PlaybackMode.UNTIL_DONE)
+    if receivedString == "START":
+        basic.show_string("START")
+        drawShips()
+        music.play(music.builtin_playable_sound_effect(soundExpression.happy),
+            music.PlaybackMode.UNTIL_DONE)
 radio.on_received_string(on_received_string)
 
 def on_button_pressed_b():
@@ -35,9 +60,25 @@ def on_button_pressed_b():
     B += 1
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
+def on_received_value(name, value):
+    global shotX, shotY
+    basic.clear_screen()
+    for x4 in range(5):
+        for y4 in range(5):
+            if abArray[x4][y4] == value:
+                shotX = x4
+                shotY = y4
+    led.plot(shotX, shotY)
+radio.on_received_value(on_received_value)
+
+shotY = 0
+shotX = 0
+abArray: List[List[number]] = []
 isPhase2 = False
 B = 0
+array: List[List[bool]] = []
 A = 0
 isFirstPlayer = False
+lista: List[number] = []
 radio.set_group(1)
-isFirstPlayer = True
+isFirstPlayer = False
--- main.ts
+++ main.ts
@@ -3,34 +3,83 @@ input.onButtonPressed(Button.A, function () {
 })
 function copyShips () {
     for (let x = 0; x <= 4; x++) {
+        array[x] = []
         for (let y = 0; y <= 4; y++) {
-        	
+            array[x][y] = led.point(x, y)
+        }
+    }
+}
+function drawShips () {
+    for (let x = 0; x <= 4; x++) {
+        for (let y = 0; y <= 4; y++) {
+            if (array[x][y]) {
+                led.plot(x, y)
+            }
         }
     }
 }
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
     led.toggle(A, B)
-    B = 0
-    A = 0
 })
 input.onButtonPressed(Button.AB, function () {
-    radio.sendString("ooo")
+    if (isPhase2) {
+        radio.sendValue("SHOT", 5 * B + A)
+    }
+    radio.sendString("READY")
+    copyShips()
+    createABArray()
+    basic.clearScreen()
+    isPhase2 = true
     A = 0
     B = 0
-    isPhase2 = true
-    copyShips()
 })
+function createABArray () {
+    for (let x = 0; x <= 4; x++) {
+        abArray[x] = []
+        for (let y = 0; y <= 4; y++) {
+            abArray[x][y] = 5 * y + x
+        }
+    }
+}
 radio.onReceivedString(function (receivedString) {
     if (receivedString == "READY") {
-        isFirstPlayer = false
+        music.play(music.builtinPlayableSoundEffect(soundExpression.hello), music.PlaybackMode.UntilDone)
+        if (isPhase2) {
+            isFirstPlayer = true
+            radio.sendString("START")
+            basic.showString("START")
+            music.play(music.builtinPlayableSoundEffect(soundExpression.happy), music.PlaybackMode.UntilDone)
+        }
+    }
+    if (receivedString == "START") {
+        basic.showString("START")
+        drawShips()
+        music.play(music.builtinPlayableSoundEffect(soundExpression.happy), music.PlaybackMode.UntilDone)
     }
 })
 input.onButtonPressed(Button.B, function () {
     B += 1
 })
+radio.onReceivedValue(function (name, value) {
+    basic.clearScreen()
+    for (let x = 0; x <= 4; x++) {
+        for (let y = 0; y <= 4; y++) {
+            if (abArray[x][y] == value) {
+                shotX = x
+                shotY = y
+            }
+        }
+    }
+    led.plot(shotX, shotY)
+})
+let shotY = 0
+let shotX = 0
+let abArray: number[][] = []
 let isPhase2 = false
 let B = 0
+let array: boolean[][] = []
 let A = 0
 let isFirstPlayer = false
+let lista: number[] = []
 radio.setGroup(1)
-isFirstPlayer = true
+isFirstPlayer = false
