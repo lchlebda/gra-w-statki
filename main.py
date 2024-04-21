53cc6561db1982feacf8fe4e6942b5a74af99040
aitseb <lchlebda@gmail.com>
2024-04-21 16:52:35 +0200

    faza druga - strzelanie

--- main.py
+++ main.py
@@ -1,44 +1,61 @@
+def copyHits():
+    for x in range(5):
+        arrayHits[x] = []
+        for y in range(5):
+            arrayHits[x][y] = led.point_brightness(x, y)
+
 def on_button_pressed_a():
     global A
     A += 1
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
 def copyShips():
-    for x in range(5):
-        array[x] = []
-        for y in range(5):
-            array[x][y] = led.point(x, y)
-def drawShips():
     for x2 in range(5):
+        arrayShips[x2] = []
         for y2 in range(5):
-            if array[x2][y2]:
-                led.plot(x2, y2)
+            arrayShips[x2][y2] = led.point(x2, y2)
+def drawShips():
+    for x22 in range(5):
+        for y22 in range(5):
+            if arrayShips[x22][y22]:
+                led.plot(x22, y22)
 
 def on_logo_pressed():
-    led.toggle(A, B)
+    global A, B
+    if not (isPhase2):
+        led.toggle(A, B)
+        A = 0
+        B = 0
 input.on_logo_event(TouchButtonEvent.PRESSED, on_logo_pressed)
+
+def drawHits():
+    for x23 in range(5):
+        for y23 in range(5):
+            led.plot_brightness(x23, y23, arrayHits[x23][y23])
 
 def on_button_pressed_ab():
     global isPhase2, A, B
     if isPhase2:
         radio.send_value("SHOT", 5 * B + A)
-    radio.send_string("READY")
-    copyShips()
-    createABArray()
-    basic.clear_screen()
-    isPhase2 = True
-    A = 0
-    B = 0
+    else:
+        radio.send_string("READY")
+        copyShips()
+        createABArray()
+        basic.clear_screen()
+        copyHits()
+        isPhase2 = True
+        A = 0
+        B = 0
 input.on_button_pressed(Button.AB, on_button_pressed_ab)
 
 def createABArray():
-    for y3 in range(5):
-        abArray[y3] = []
-        for x3 in range(5):
-            abArray[y3][x3] = 5 * y3 + x3
+    for x3 in range(5):
+        abArray[x3] = []
+        for y3 in range(5):
+            abArray[x3][y3] = 5 * y3 + x3
 
 def on_received_string(receivedString):
-    global isFirstPlayer
+    global isFirstPlayer, A, B
     if receivedString == "READY":
         music.play(music.builtin_playable_sound_effect(soundExpression.hello),
             music.PlaybackMode.UNTIL_DONE)
@@ -53,6 +70,20 @@ def on_received_string(receivedString):
         drawShips()
         music.play(music.builtin_playable_sound_effect(soundExpression.happy),
             music.PlaybackMode.UNTIL_DONE)
+    if receivedString == "HIT":
+        music.play(music.builtin_playable_sound_effect(soundExpression.happy),
+            music.PlaybackMode.UNTIL_DONE)
+        led.plot(A, B)
+    if receivedString == "MISSED":
+        music.play(music.builtin_playable_sound_effect(soundExpression.sad),
+            music.PlaybackMode.UNTIL_DONE)
+        led.plot_brightness(A, B, 100)
+    if receivedString == "MISSED" or receivedString == "HIT":
+        copyHits()
+        basic.clear_screen()
+        drawShips()
+        A = 0
+        B = 0
 radio.on_received_string(on_received_string)
 
 def on_button_pressed_b():
@@ -62,23 +93,27 @@ input.on_button_pressed(Button.B, on_button_pressed_b)
 
 def on_received_value(name, value):
     global shotX, shotY
-    basic.clear_screen()
     for x4 in range(5):
         for y4 in range(5):
             if abArray[x4][y4] == value:
                 shotX = x4
                 shotY = y4
-    led.plot(shotX, shotY)
+    if arrayShips[shotX][shotY]:
+        radio.send_string("HIT")
+    else:
+        radio.send_string("MISSED")
+    basic.clear_screen()
+    drawHits()
 radio.on_received_value(on_received_value)
 
 shotY = 0
 shotX = 0
 abArray: List[List[number]] = []
-isPhase2 = False
 B = 0
-array: List[List[bool]] = []
+isPhase2 = False
+arrayShips: List[List[bool]] = []
 A = 0
+arrayHits: List[List[number]] = []
 isFirstPlayer = False
-lista: List[number] = []
 radio.set_group(1)
 isFirstPlayer = False
--- main.ts
+++ main.ts
@@ -1,43 +1,64 @@
+function copyHits () {
+    for (let x = 0; x <= 4; x++) {
+        arrayHits[x] = []
+        for (let y = 0; y <= 4; y++) {
+            arrayHits[x][y] = led.pointBrightness(x, y)
+        }
+    }
+}
 input.onButtonPressed(Button.A, function () {
     A += 1
 })
 function copyShips () {
     for (let x = 0; x <= 4; x++) {
-        array[x] = []
+        arrayShips[x] = []
         for (let y = 0; y <= 4; y++) {
-            array[x][y] = led.point(x, y)
+            arrayShips[x][y] = led.point(x, y)
         }
     }
 }
 function drawShips () {
-    for (let x = 0; x <= 4; x++) {
-        for (let y = 0; y <= 4; y++) {
-            if (array[x][y]) {
-                led.plot(x, y)
+    for (let x2 = 0; x2 <= 4; x2++) {
+        for (let y2 = 0; y2 <= 4; y2++) {
+            if (arrayShips[x2][y2]) {
+                led.plot(x2, y2)
             }
         }
     }
 }
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
-    led.toggle(A, B)
+    if (!(isPhase2)) {
+        led.toggle(A, B)
+        A = 0
+        B = 0
+    }
 })
+function drawHits () {
+    for (let x2 = 0; x2 <= 4; x2++) {
+        for (let y2 = 0; y2 <= 4; y2++) {
+            led.plotBrightness(x2, y2, arrayHits[x2][y2])
+        }
+    }
+}
 input.onButtonPressed(Button.AB, function () {
     if (isPhase2) {
         radio.sendValue("SHOT", 5 * B + A)
+    } else {
+        radio.sendString("READY")
+        copyShips()
+        createABArray()
+        basic.clearScreen()
+        copyHits()
+        isPhase2 = true
+        A = 0
+        B = 0
     }
-    radio.sendString("READY")
-    copyShips()
-    createABArray()
-    basic.clearScreen()
-    isPhase2 = true
-    A = 0
-    B = 0
 })
 function createABArray () {
-    for (let x = 0; x <= 4; x++) {
-        abArray[x] = []
-        for (let y = 0; y <= 4; y++) {
-            abArray[x][y] = 5 * y + x
+    for (let x3 = 0; x3 <= 4; x3++) {
+        abArray[x3] = []
+        for (let y3 = 0; y3 <= 4; y3++) {
+            abArray[x3][y3] = 5 * y3 + x3
         }
     }
 }
@@ -56,30 +77,50 @@ radio.onReceivedString(function (receivedString) {
         drawShips()
         music.play(music.builtinPlayableSoundEffect(soundExpression.happy), music.PlaybackMode.UntilDone)
     }
+    if (receivedString == "HIT") {
+        music.play(music.builtinPlayableSoundEffect(soundExpression.happy), music.PlaybackMode.UntilDone)
+        led.plot(A, B)
+    }
+    if (receivedString == "MISSED") {
+        music.play(music.builtinPlayableSoundEffect(soundExpression.sad), music.PlaybackMode.UntilDone)
+        led.plotBrightness(A, B, 100)
+    }
+    if (receivedString == "MISSED" || receivedString == "HIT") {
+        copyHits()
+        basic.clearScreen()
+        drawShips()
+        A = 0
+        B = 0
+    }
 })
 input.onButtonPressed(Button.B, function () {
     B += 1
 })
 radio.onReceivedValue(function (name, value) {
-    basic.clearScreen()
-    for (let x = 0; x <= 4; x++) {
-        for (let y = 0; y <= 4; y++) {
-            if (abArray[x][y] == value) {
-                shotX = x
-                shotY = y
+    for (let x4 = 0; x4 <= 4; x4++) {
+        for (let y4 = 0; y4 <= 4; y4++) {
+            if (abArray[x4][y4] == value) {
+                shotX = x4
+                shotY = y4
             }
         }
     }
-    led.plot(shotX, shotY)
+    if (arrayShips[shotX][shotY]) {
+        radio.sendString("HIT")
+    } else {
+        radio.sendString("MISSED")
+    }
+    basic.clearScreen()
+    drawHits()
 })
 let shotY = 0
 let shotX = 0
 let abArray: number[][] = []
-let isPhase2 = false
 let B = 0
-let array: boolean[][] = []
+let isPhase2 = false
+let arrayShips: boolean[][] = []
 let A = 0
+let arrayHits: number[][] = []
 let isFirstPlayer = false
-let lista: number[] = []
 radio.setGroup(1)
 isFirstPlayer = false
